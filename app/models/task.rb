ddee40a98cbc34aef2cedb2435d9ae8d94f58b44
Mafe Beltran <mfbeltran@uninorte.edu.co>
2015-11-10 04:55:07 +0000

    Fix validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ActiveRecord::Base
-    
+    validates :title, :description, :duration, :start_date, :end_date, presence:true
     belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :username, :first_name, :laste_name, :email, presence:true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
     
